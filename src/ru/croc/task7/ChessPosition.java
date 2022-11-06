7bac65ab660e8c01b5713d20aee095898d59201f
karluklaru <ninopetrochenko@yandex.ru>
2022-11-06 19:36:17 +0300

    Changed task7.

--- src/ru/croc/task7/ChessPosition.java
+++ src/ru/croc/task7/ChessPosition.java
@@ -13,7 +13,7 @@ public class ChessPosition {
     }
 
     public static ChessPosition parse(String position) throws IllegalPositionException {
-        return new ChessPosition(letToNum(position.charAt(0)), Character.getNumericValue(position.charAt(1)) - 1);
+        return new ChessPosition(position.charAt(0) - 'a', Character.getNumericValue(position.charAt(1)) - 1);
     }
 
     public static String knightMoving(ChessPosition... chessPositions) throws IllegalMoveException {
@@ -27,45 +27,13 @@ public class ChessPosition {
 
     @Override
     public String toString() {
-        try {
-            return numToLet(x) + (y + 1);
-        } catch (IllegalPositionException e) {
-            throw new RuntimeException(e);
-        }
+        return Character.toString('a' + x) + (y + 1);
     }
 
     private static boolean checkKnightsMove(ChessPosition before, ChessPosition after) {
         int subtractOfX = Math.abs(before.x - after.x);
         int subtractOfY = Math.abs(before.y - after.y);
         return (subtractOfX == 1 || subtractOfX == 2) && (subtractOfY == 1 || subtractOfY == 2);
-    }
-
-    private String numToLet(int num) throws IllegalPositionException {
-        return switch (num) {
-            case 0 -> "a";
-            case 1 -> "b";
-            case 2 -> "c";
-            case 3 -> "d";
-            case 4 -> "e";
-            case 5 -> "f";
-            case 6 -> "g";
-            case 7 -> "h";
-            default -> throw new IllegalPositionException();
-        };
-    }
-
-    private static int letToNum(char let) throws IllegalPositionException {
-        return switch (let) {
-            case 'a' -> 0;
-            case 'b' -> 1;
-            case 'c' -> 2;
-            case 'd' -> 3;
-            case 'e' -> 4;
-            case 'f' -> 5;
-            case 'g' -> 6;
-            case 'h' -> 7;
-            default -> throw new IllegalPositionException();
-        };
     }
 
 }
