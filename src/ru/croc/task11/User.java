3ee4673308df269aed58eb57c84d5fed272dd239
karluklaru <ninopetrochenko@yandex.ru>
2022-11-15 16:49:50 +0300

    First variant of task 11.

--- src/ru/croc/task11/User.java
+++ src/ru/croc/task11/User.java
@@ -14,17 +14,17 @@ public class User extends Thread {
     @Override
     public void run() {
         boolean check;
-        int rn = 0;
+        int randomRate = 0;
         while (lot.getAuctionEnd().compareTo(LocalDateTime.now()) > 0) {
             try {
                 Thread.sleep((long) (Math.random() * 1000));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            rn += (int) (Math.random()*10);
-            check = lot.updateRate(userName, rn);
+            randomRate += (int) (Math.random()*10);
+            check = lot.updateRate(userName, randomRate);
             if (check) {
-                System.out.println("New rate! " + userName + " updating rate to " + rn);
+                System.out.println("New rate! " + userName + " updating rate to " + randomRate);
             }
         }
     }
