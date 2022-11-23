1be1eee1ab309a4b706c29e17549da4bd76da00c
karluklaru <ninopetrochenko@yandex.ru>
2022-11-23 13:53:51 +0300

    Fixed algorithm in step 3 and created directory for films list and viewed list.

--- src/ru/croc/task13/Main.java
+++ src/ru/croc/task13/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        RecommendAlgorithm recommendAlgorithm = new RecommendAlgorithm("C:\\Users\\ninop\\IdeaProjects\\java-school-2022\\src\\ru\\croc\\task13\\films.txt",
-                "C:\\Users\\ninop\\IdeaProjects\\java-school-2022\\src\\ru\\croc\\task13\\viewed.txt");
         System.out.println("Введите индексы просмотренных фильмов через запятую");
         String line = sc.nextLine();
-        String[] films = line.split(",");
-        System.out.println(recommendAlgorithm.getRecommendFilm(films));
+
+        RecommendAlgorithm recommendAlgorithm = new RecommendAlgorithm("C:\\Users\\ninop\\IdeaProjects\\java-school-2022\\src\\ru\\croc\\task13\\resources\\films.txt",
+                "C:\\Users\\ninop\\IdeaProjects\\java-school-2022\\src\\ru\\croc\\task13\\resources\\viewed.txt", line);
+        System.out.println(recommendAlgorithm.getRecommendFilm());
     }
 }
--- src/ru/croc/task13/RecommendAlgorithm.java
+++ src/ru/croc/task13/RecommendAlgorithm.java
@@ -8,27 +8,35 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class RecommendAlgorithm {
-    private final Map<String, String> filmDataBase;
-    private final List<List<String>> viewedDataBase;
+    private final Integer[] userViewed;
+    private final Map<Integer, String> filmDataBase;
+    private final List<List<Integer>> viewedDataBase;
 
-    public RecommendAlgorithm(String pathToFilms, String pathToViewed) {
+    public RecommendAlgorithm(String pathToFilms, String pathToViewed, String viewed) {
+        userViewed = getUserFilms(viewed);
         filmDataBase = getFilmsIntoFile(pathToFilms);
         viewedDataBase = getViewedFromFile(pathToViewed);
     }
 
-    public String getRecommendFilm(String... films) {
-        String key = mostPopularFilm(excludeViewed(getSimilarUsers(films), films));
+    public String getRecommendFilm() {
+        Integer key = mostPopularFilm(excludeViewed(getSimilarUsers()));
         return filmDataBase.get(key);
     }
 
-    private List<List<String>> getSimilarUsers(String... films) {
-        List<List<String>> similarUsers = new ArrayList<>();
-        for (List<String> oneUserViewed : viewedDataBase) {
+    /**
+     * Метод, получающий список пользователей сервиса, предпочтения которых похожи на предпочтения
+     * конкретного пользователя
+     * @return список пользователей, которые просмотрели хотя бы половину данного списка
+     */
+    private List<List<Integer>> getSimilarUsers() {
+        Set<Integer> uniqueFilms = new HashSet<>(List.of(userViewed));
+        List<List<Integer>> similarUsers = new ArrayList<>();
+        for (List<Integer> oneUserViewed : viewedDataBase) {
             int count = 0;
-            for (String film : films) {
+            for (Integer film : uniqueFilms) {
                 if (oneUserViewed.contains(film)) {
                     ++count;
-                    if (count == films.length/2) {
+                    if (count == userViewed.length/2) {
                         similarUsers.add(oneUserViewed);
                         break;
                     }
@@ -38,50 +46,58 @@ public class RecommendAlgorithm {
         return similarUsers;
     }
 
-    private List<String> excludeViewed(List<List<String>> similarUsers, String... films) {
-         List<String> notViewedFilms = new ArrayList<>();
-        for (List<String> user : similarUsers) {
-            List<String> u = new ArrayList<>(user);
-            u.removeAll(List.of(films));
+    /**
+     * @param similarUsers пользователи, отобранные алгоритмом как "похожие по предпочтениям"
+     * @return список уникальных фильмов, которые исследуемый пользователь еще не просмотрел
+     */
+    private Set<Integer> excludeViewed(List<List<Integer>> similarUsers) {
+        Set<Integer> notViewedFilms = new HashSet<>();
+        for (List<Integer> user : similarUsers) {
+            List<Integer> u = new ArrayList<>(user);
+            u.removeAll(List.of(userViewed));
             notViewedFilms.addAll(u);
         }
         return notViewedFilms;
     }
 
-    private String mostPopularFilm(List<String> films) {
-        Map<String, Integer> filmFrequency = getFilmFrequency(films);
+    /**
+     * @param films список фильмов, отобранных на основе похожих пользователей и еще не просмотренных
+     *              исследуемым.
+     * @return ключ самого популярного по просмотрам фильма из списка среди всех пользователей сервиса
+     */
+    private Integer mostPopularFilm(Set<Integer> films) {
+        Map<Integer, Integer> filmFrequency = getFilmFrequency(films);
         Collection<Integer> frequencies = filmFrequency.values();
-        int max = 0;
-        for (Integer freq : frequencies) {
-            if (freq > max) {
-                max = freq;
-            }
-        }
+        int max = Collections.max(frequencies);
         return getKey(filmFrequency, max);
     }
 
-    private Map<String, Integer> getFilmFrequency(List<String> films) {
-        Map<String, Integer> filmFrequency = new HashMap<>();
-        for (String film : films) {
-            Integer count = filmFrequency.get(film);
-            if (count == null) {
-                count = 0;
+    /**
+     * @param films список уникальных фильмов похожих пользователей, которые исследуемый еще не просмотрел
+     * @return Список фильмов, где каждому индексу фильма соответствует количество его просмотров у всех
+     * пользователей сервиса
+     */
+    private Map<Integer, Integer> getFilmFrequency(Set<Integer> films) {
+        Map<Integer, Integer> filmFrequency = new HashMap<>();
+        for (Integer film : films) {
+            int count = 0;
+            for (List<Integer> oneUserViewed : viewedDataBase) {
+                count += Collections.frequency(oneUserViewed, film);
             }
-            ++count;
             filmFrequency.put(film, count);
         }
         return filmFrequency;
     }
 
-    private Map<String, String> getFilmsIntoFile(String pathToFilms) {
-        Map<String, String> films = new HashMap<>();
+    private Map<Integer, String> getFilmsIntoFile(String pathToFilms) {
+        Map<Integer, String> films = new HashMap<>();
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream(pathToFilms), StandardCharsets.UTF_8))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                String[] film = line.split(",");
-                films.put(film[0], film[1]);
+                String[] position = line.split(",");
+                films.put(Integer.parseInt(position[0]), position[1]);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -89,20 +105,27 @@ public class RecommendAlgorithm {
         return films;
     }
 
-    private List<List<String>> getViewedFromFile(String pathToViewed) {
-        List<List<String>> viewed = new ArrayList<>();
+    private List<List<Integer>> getViewedFromFile(String pathToViewed) {
+        List<List<Integer>> viewed = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(
                         new FileInputStream(pathToViewed), StandardCharsets.UTF_8))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] oneUserViewed = line.split(",");
-                viewed.add(List.of(oneUserViewed));
+                Integer[] keys = strArrToIntegerArr(oneUserViewed);
+                viewed.add(List.of(keys));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return viewed;
+    }
+
+    private Integer[] getUserFilms(String userFilms) {
+        String[] divided = userFilms.split(",");
+        return strArrToIntegerArr(divided);
+
     }
 
     private <K, V> K getKey(Map<K, V> map, V value) {
@@ -113,4 +136,16 @@ public class RecommendAlgorithm {
         }
         return null;
     }
+
+    private Integer[] strArrToIntegerArr(String... strArray) {
+        int i = 0;
+        Integer[] intArray = new Integer[strArray.length];
+        for (String f : strArray) {
+            intArray[i] = Integer.parseInt(f);
+            ++i;
+        }
+        return intArray;
+    }
+
+
 }
