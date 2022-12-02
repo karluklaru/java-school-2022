f83d1f75a7ca710fb22216f57a21de1f8e0e66d0
karluklaru <ninopetrochenko@yandex.ru>
2022-12-02 14:55:17 +0300

    fix: divided class into two classes: the first work with databases of films and users, the second implements recommend algorithm. fix: change condition to find recommend film if user watched one film

--- src/ru/croc/task13/Main.java
+++ src/ru/croc/task13/Main.java
@@ -8,8 +8,10 @@ public class Main {
         System.out.println("Введите индексы просмотренных фильмов через запятую");
         String line = sc.nextLine();
 
-        RecommendAlgorithm recommendAlgorithm = new RecommendAlgorithm("C:\\Users\\ninop\\IdeaProjects\\java-school-2022\\src\\ru\\croc\\task13\\resources\\films.txt",
+        DatabaseWorker databaseWorker = new DatabaseWorker("C:\\Users\\ninop\\IdeaProjects\\java-school-2022\\src\\ru\\croc\\task13\\resources\\films.txt",
                 "C:\\Users\\ninop\\IdeaProjects\\java-school-2022\\src\\ru\\croc\\task13\\resources\\viewed.txt", line);
+        RecommendAlgorithm recommendAlgorithm = new RecommendAlgorithm(databaseWorker);
         System.out.println(recommendAlgorithm.getRecommendFilm());
+
     }
 }
