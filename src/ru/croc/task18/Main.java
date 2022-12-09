fb2519f8113f8b5af39ab2e4104fbec350a858bd
karluklaru <ninopetrochenko@yandex.ru>
2022-12-09 18:41:19 +0300

    fix: divided one dao class into two and changed main

--- src/ru/croc/task18/Main.java
+++ src/ru/croc/task18/Main.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Dao dao = new Dao();
-
-        System.out.println(dao.findProduct("Т9"));
-        System.out.println(dao.createProduct(new Product("Т9", "Колонка", 3000)));
+        ProductDao productDao = new ProductDao();
+        OrderDao orderDao = new OrderDao();
+        System.out.println(productDao.findProduct("Т9"));
+        System.out.println(productDao.createProduct(new Product("Т9", "Колонка", 3000)));
         List<Product> products = new ArrayList<>();
         products.add(new Product("Т15", "Колонка", 4000));
         products.add(new Product("Т12", "Наушники", 4000));
         products.add(new Product("Т4", "Блок питания", 200));
-        dao.createOrder("nina", products);
+        orderDao.createOrder("nina", products);
 
     }
 }
